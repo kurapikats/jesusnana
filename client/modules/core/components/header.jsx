5dbbd7407f55afdc1b789a52f1d4b9639b2293e4
kurapikats <kurapikats@yahoo.com>
2017-01-27 23:55:52 +0800

    move drawer to header

--- client/modules/core/components/header.jsx
+++ client/modules/core/components/header.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
+// import SideNav from './side_nav';
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      open: false
+    }
   }
 
+  handleNavClick() {
+    this.setState({
+      open: !this.state.open
+    });
+  }
+
+
+
   render() {
+    // <SideNav open={this.state.open} />
+
     return (
-      <AppBar
-        title="Jesus B. Nana"
-        iconClassNameRight="muidocs-icon-navigation-expand-more"
-      />
+      <div>
+        <AppBar
+          title="Jesus B. Nana"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+          onTouchTap={this.handleNavClick.bind(this)}
+        />
+        <Drawer
+          onRequestChange={(open) => this.setState({open})}
+          open={this.state.open}
+          docked={false}
+        >
+          <MenuItem>TaxiComplaints</MenuItem>
+          <MenuItem>Percentage Calc</MenuItem>
+          <MenuItem>CV</MenuItem>
+        </Drawer>
+      </div>
     );
   }
 }
