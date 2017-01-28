b35288a4ad63b20d392e662fd5318ce6c36cf04f
kurapikats <kurapikats@yahoo.com>
2017-01-28 23:09:17 +0800

    improve contact form

--- client/modules/core/components/contact.jsx
+++ client/modules/core/components/contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import Paper from 'material-ui/Paper';
 
 class Contact extends React.Component {
   constructor(props) {
@@ -10,34 +11,42 @@ class Contact extends React.Component {
   render() {
     return (
       <div style={{margin: '0 15px'}}>
-        <form>
-          <TextField
-            ref="name"
-            floatingLabelText="Name"
-            required
-          /><br />
-          <TextField
-            ref="email"
-            floatingLabelText="Email"
-            required
-          /><br />
-          <TextField
-            ref="subject"
-            floatingLabelText="Subject"
-            required
-          /><br />
-          <TextField
-            ref="message"
-            floatingLabelText="Message"
-            multiline={true}
-            rows={3}
-            required
-          /><br />
-          <RaisedButton
-            label="Send"
-            primary={true}
-          />
-        </form>
+        <Paper zDepth={2} style={{margin: "20px 5px 10px 5px", padding: 10, width: 400}}>
+          <h4>Contact Onin</h4>
+          <form>
+            <TextField
+              ref="name"
+              floatingLabelText="Name"
+              fullWidth={true}
+              required
+            /><br />
+            <TextField
+              ref="email"
+              floatingLabelText="Email"
+              fullWidth={true}
+              required
+            /><br />
+            <TextField
+              ref="subject"
+              floatingLabelText="Subject"
+              fullWidth={true}
+              required
+            /><br />
+            <TextField
+              ref="message"
+              floatingLabelText="Message"
+              multiLine={true}
+              fullWidth={true}
+              rows={3}
+              required
+            /><br />
+            <RaisedButton
+              label="Send"
+              primary={true}
+              fullWidth={true}
+            />
+          </form>
+        </Paper>
       </div>
     );
   }
