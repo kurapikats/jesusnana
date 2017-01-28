cbd5b7d4a5b278921147ec21ad98ef62fd45b688
kurapikats <kurapikats@yahoo.com>
2017-01-28 23:41:22 +0800

    made components controlled

--- client/modules/core/components/contact.jsx
+++ client/modules/core/components/contact.jsx
@@ -6,6 +6,43 @@ import Paper from 'material-ui/Paper';
 class Contact extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      name: "",
+      email: "",
+      subject: "",
+      message: ""
+    }
+  }
+
+  handleNameChange(e) {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+  handleEmailChange(e) {
+    this.setState({
+      email: e.target.value
+    });
+  }
+
+  handleSubjectChange(e) {
+    this.setState({
+      subject: e.target.value
+    });
+  }
+
+  handleMessageChange(e) {
+    this.setState({
+      message: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    // let {name} = this.refs
+    // console.log(name.input.value, 'name');
+    // name.input.value = "";
   }
 
   render() {
@@ -18,23 +55,28 @@ class Contact extends React.Component {
               ref="name"
               floatingLabelText="Name"
               fullWidth={true}
+              onChange={this.handleNameChange.bind(this)}
+              value={this.state.name}
               required
             /><br />
             <TextField
               ref="email"
               floatingLabelText="Email"
+              onChange={this.handleEmailChange.bind(this)}
               fullWidth={true}
               required
             /><br />
             <TextField
               ref="subject"
               floatingLabelText="Subject"
+              onChange={this.handleSubjectChange.bind(this)}
               fullWidth={true}
               required
             /><br />
             <TextField
               ref="message"
               floatingLabelText="Message"
+              onChange={this.handleMessageChange.bind(this)}
               multiLine={true}
               fullWidth={true}
               rows={3}
@@ -44,6 +86,7 @@ class Contact extends React.Component {
               label="Send"
               primary={true}
               fullWidth={true}
+              onClick={this.handleSubmit.bind(this)}
             />
           </form>
         </Paper>
